62c858535a13f862d4f17bdf0b26782c2e57cb30
shuhei fujita <syuheifujita0117@gmail.com>
2023-07-27 22:58:35 +0900

    slack to gptのパイプラインを追加

--- gpt/call_gpt_api.py
+++ gpt/call_gpt_api.py
@@ -7,35 +7,7 @@ import json
 import logging
 import time
 
-def api_response(prompt):
-    # APIリクエスト
-    try:
-        logging.info('Starting API request...')
-        start_time = time.time()
-        response = openai.ChatCompletion.create(
-        model=engine_3,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ]
-        )
-        end_time = time.time()
-        logging.info('Finished API request in %.2f seconds.', end_time - start_time)
-    except Exception as e:
-        logging.error('Error occurred during API request: %s', e)
-        raise
-
-    # APIレスポンスを整形
-    formatted_response = json.dumps(response['choices'][0]['message'], indent=4, ensure_ascii=False)
-
-    # APIレスポンスの実行結果の出力
-    with open('result.json', 'w') as json_file:
-        json.dump(response['choices'][0]['message'], json_file, indent=4, ensure_ascii=False)
-    with open('result.txt', 'w') as txt_file:
-        txt_file.write(response['choices'][0]['message']['content'])
-    print(formatted_response)
-
-if __name__ == '__main__':
+def main(prompt):
     # ---設定ファイルの初期化---
     path_current = os.path.dirname(os.path.realpath(__file__))
 
@@ -53,4 +25,30 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
     # ---設定ファイルの初期化---
     
-    api_response(prompt)
+    # APIリクエスト
+    try:
+        logging.info('Starting API request...')
+        start_time = time.time()
+        response = openai.ChatCompletion.create(
+            model=engine_3,
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        end_time = time.time()
+        logging.info('Finished API request in %.2f seconds.', end_time - start_time)
+    except Exception as e:
+        logging.error('Error occurred during API request: %s', e)
+        raise
+
+    # APIレスポンスを整形
+    formatted_response = json.dumps(response['choices'][0]['message'], indent=4, ensure_ascii=False)
+
+    # APIレスポンスの実行結果の出力
+    with open('result.json', 'w') as json_file:
+        json.dump(response['choices'][0]['message'], json_file, indent=4, ensure_ascii=False)
+    with open('result.txt', 'w') as txt_file:
+        txt_file.write(response['choices'][0]['message']['content'])
+
+    return formatted_response
---
+++ gpt/call_gpt_to_slack.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+import json
+
+sys.path.insert(0, '../gpt')
+import call_gpt_api
+sys.path.insert(0, '../slack')
+import call_slack_api
+
+# ---設定ファイルの初期化---
+path_current = os.path.dirname(os.path.realpath(__file__))
+path_prompt = os.path.join(path_current, 'prompt.md')
+with open(path_prompt, 'r') as file:
+    prompt = file.read()
+
+response = call_gpt_api.main(prompt)
+data = json.loads(response)
+content = data['content']
+formatted_content = content.replace("\\n", "\n")
+
+call_slack_api.main(formatted_content)
