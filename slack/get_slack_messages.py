d2a7aa9e23a440472488210e5450515e0c6588a2
shuhei fujita <syuheifujita0117@gmail.com>
2023-07-28 02:11:46 +0900

    update

--- slack/get_slack_messages.py
+++ slack/get_slack_messages.py
@@ -8,9 +8,13 @@ slack_token = os.environ['SLACK_API_TOKEN']
 slack_channel_id = os.environ['SLACK_CHANNEL_ID']
 client = WebClient(token=slack_token)
 
+# 上限あり
 def get_messages_in_thread():
     # Slackのチャンネル内のメッセージ履歴をすべて取得
-    response_conversations = client.conversations_history(channel=slack_channel_id)
+    response_conversations = client.conversations_history(
+        channel=slack_channel_id,
+        limit=100
+    )
     with open('slack_messages.txt', 'w') as f:
         for message in response_conversations.data['messages']:
             f.write(f"{message['text']}\n")
@@ -18,5 +22,35 @@ def get_messages_in_thread():
         dt_now = datetime.datetime.now()
         f.write(f"{dt_now}\n")
 
+# 上限なし
+def get_messages_in_thread_no_limit():
+    all_messages = []
+    response = client.conversations_history(
+        channel=slack_channel_id,
+        limit=1000
+    )
+    all_messages.extend(response['messages'])
+
+    # Check if more messages are available
+    while response['has_more']:
+        # Get the cursor for the next page of messages
+        cursor = response['response_metadata']['next_cursor']
+
+        # Get the next page of messages
+        response = client.conversations_history(
+            channel=slack_channel_id,
+            cursor=cursor,
+            limit=1000
+        )
+        all_messages.extend(response['messages'])
+
+    with open('slack_messages.txt', 'w') as f:
+        for message in all_messages:
+            f.write(f"{message['text']}\n")
+        
+        dt_now = datetime.datetime.now()
+        f.write(f"{dt_now}\n")
+
 if __name__ == "__main__":
-    get_messages_in_thread()
+    # get_messages_in_thread()
+    get_messages_in_thread_no_limit()
