a7c763714b217eb7436d3f286cc018ee44b7cbbc
shuhei fujita <fujitash@ns.aucnet.co.jp>
2023-08-04 09:23:15 +0900

    update

--- github/call_github_api.py
+++ github/call_github_api.py
@@ -11,10 +11,10 @@ develop = repo.heads.develop
 # 差分を取得
 diff_index = main.commit.diff(develop.commit)
 
-# 差分の各ファイルに対してチェックを行う
-for diff_item in diff_index.iter_change_type('M'):  # 'M'は変更されたファイルを意味します
-    if diff_item.a_path.endswith('.py'):
-        checker = Checker(diff_item.a_path)
-        errors = checker.check_all()
-        if errors > 0:
-            print(f"{errors} errors found in {diff_item.a_path}")
+# 出力をテキストファイルにリダイレクト
+# 出力をテキストファイルにリダイレクト
+with open('result.txt', 'w') as f:
+    # 差分の各ファイルに対してチェックを行う
+    for diff_item in diff_index.iter_change_type('M'):  # 'M'は変更されたファイルを意味します
+        # ファイル名を出力
+        print(f"Modified file: {diff_item.a_path}", file=f)
